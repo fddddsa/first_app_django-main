f075fb8c9ff841eec3c8906a987b06c208e0ea7c
Your Name <you@example.com>
2024-10-14 11:15:22 +0300

    завершил лз 6

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -87,3 +87,18 @@ def m500(request):
 
 def index(request):
  return render(request, "firstapp/home.html")
+
+def index(request):
+ # return render(request, "firstapp/home.html")
+ data = {"header": "Передача параметров в шаблон Django",
+ "message": "Загружен шаблон templates/firstapp/index_app1.html"}
+ return render(request, "firstapp/index_app1.html", context=data)
+
+
+def get_data(request):
+    header = "Персональные данные" # обычная переменная
+    langs = ["Английский", "Немецкий", "Испанский"] # массив
+    user = {"name": "Максим", "age": 30} # словарь
+    addr = ("Виноградная", 23, 45) # кортеж
+    data = {"header": header, "langs": langs, "user": user, "address": addr}
+    return render(request, "index.html", context=data)
