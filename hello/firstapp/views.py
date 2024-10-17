68298e92ba0422c19ff6d2d978d85f70669ac84b
Your Name <you@example.com>
2024-10-17 14:11:29 +0300

    лз 7 Сделал вывод страницы home.html с использованием внутреннего стиля

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -89,10 +89,8 @@ def index(request):
  return render(request, "firstapp/home.html")
 
 def index(request):
- # return render(request, "firstapp/home.html")
- data = {"header": "Передача параметров в шаблон Django",
- "message": "Загружен шаблон templates/firstapp/index_app1.html"}
- return render(request, "firstapp/index_app1.html", context=data)
+ return render(request, "firstapp/home.html")
+
 
 
 def get_data(request):
